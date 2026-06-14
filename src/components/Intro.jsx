0464d05dfc905da46686dd52561704f454a8b651
Abdulsf26 <abdulsf26@example.com>
2026-06-15 01:26:49 +0530

    Fix case-sensitive import for Profile.png

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { motion } from 'motion/react'
-import Me from '../assets/Images/profile.png'
+import Me from '../assets/Images/Profile.png'
 
 
 const Box = styled(motion.div)`
